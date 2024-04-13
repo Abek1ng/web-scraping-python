725f5f870d33f4b8a4d9e655b99ca6c474a4841d
abekesalimzhanov <salimzhanovabylay1@gmail.com>
2024-04-14 04:58:56 +0500

    new

--- code/halyk_scraper.py
+++ code/halyk_scraper.py
@@ -1,23 +1,36 @@
 from flask import Flask, request, jsonify
 from api.halyk_category import make_api_call
+from bs4 import BeautifulSoup
+import re
 
 app = Flask(__name__)
 
+
+def parse_string(string):
+    soup = BeautifulSoup(string, 'html.parser')
+    normal_string = soup.body.get_text(strip=True)
+    return normal_string
+   
+
 def process_and_store_category_data(category_code, data):
     company_list = []
-    for company in data:
+    for company in data.get('data'):
         name = company.get('name')
         category_name = company.get('category_name')
         description = None
         bonus = None
-        for tag in company.get('tags'):
-            bonus = tag.get('text')
-            description = tag.get('description')
+        
+        text = company.get('tags')[0].get('text')
             
+        match = re.search(r"([-+]?[0-9]*\.?[0-9]+)", text)
+        if match:
+            bonus = float(match.group(1))
+        description = parse_string(company.get('tags')[0].get('description'))
+    
         company_data = {
             'name': name,
             'category_name': category_name,
-            'description': description,
+            'description': description + "при оплате QR и смартфоном",
             'bonus': bonus
         }
 
@@ -33,7 +46,7 @@ def get_company_list(bearer_token):
         code_and_id_list = [item['code'] for item in result]
 
         for category in code_and_id_list:
-            data_path = f"/halykclub-api/v1/terminal/devices?category_code={category}&filter="
+            data_path = f"/halykclub-api/v1/terminal/merchants?category_code={category}&filter="
             data = make_api_call(bearer_token, data_path)
             company_by_category = process_and_store_category_data(category, data)
             company_list.extend(company_by_category)
